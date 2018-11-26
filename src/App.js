39f7389f0b3e65a4dc9f9b196b7493041dc5ce68
geekplux <geekplux@gmail.com>
2018-11-26 23:26:59 +0800

    first blood

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>D3 Experiment</h1>
+        <div className="container">
+          <h2>D3.js + Immutable.js performance test</h2>
+        </div>
       </div>
     );
   }
