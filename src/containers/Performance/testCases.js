529e5e8dec61bc00cefc3b3beab2cf782b3bebea
geekplux <geekplux@gmail.com>
2018-11-27 02:48:43 +0800

    WIP: test Immutable

--- src/containers/Performance/testCases.js
+++ src/containers/Performance/testCases.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
+import Immutable from 'immutable';
 
-export default [testJSInt, testJSFloat, testJSObj];
+export default [testJSInt, testImmutableInt, testJSFloat, testJSObj];
 
 export function testJSInt(tester, svg) {
   const spec = {
@@ -76,6 +77,32 @@ export function testJSObj(tester, svg) {
   spec.items[1].time= tester(spec.items[1].desc, { svg, data, key: index, xAccessor: accessor });
   spec.items[2].time = tester(spec.items[2].desc, { svg, data, key: accessor, xAccessor: accessor });
   spec.items[3].time = tester(spec.items[3].desc, { svg, data, key: ref, xAccessor: accessor });
+
+  return spec;
+};
+
+export function testImmutableInt(tester, svg) {
+  const spec = {
+    title: 'Testing Immutable IndexedSeq with 1000 Object elements',
+    items: [
+      { desc: 'default join', },
+      { desc: 'join on index key', },
+      { desc: 'join on value key', },
+    ],
+  };
+
+  console.debug(spec.title);
+  console.debug('***************************************************');
+
+  var data = [].concat([ Immutable.fromJS(d3.range(0, 1000))]);
+  console.log(data)
+
+  var index = function(d, i) { return i; };
+  var accessor = function(d, i) { return d; };
+
+  spec.items[0].time = tester(spec.items[0].desc, { svg, data, key: undefined, xAccessor: accessor });
+  spec.items[1].time= tester(spec.items[1].desc, { svg, data, key: index, xAccessor: accessor });
+  spec.items[2].time = tester(spec.items[2].desc, { svg, data, key: accessor, xAccessor: accessor });
 
   return spec;
 };
